1e42bf936b30adce317de2b11a566ca72fc1419b
daikon <daikon.tonkore@gmail.com>
2020-04-01 12:24:18 +0000

    Delete @if form favorite_blade

--- resources/views/favorites/favorite_button.blade.php
+++ resources/views/favorites/favorite_button.blade.php
@@ -1,11 +1,9 @@
-@if (Auth::id() != $micropost->id)
-    @if (Auth::user()->is_registering($micropost->id))
-        {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-secondary btn-sm"]) !!}
-        {!! Form::close() !!}
-    @else
-        {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm"]) !!}
-        {!! Form::close() !!}
-    @endif
+@if (Auth::user()->is_registering($micropost->id))
+    {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+        {!! Form::submit('Unfavorite', ['class' => "btn btn-secondary btn-sm"]) !!}
+    {!! Form::close() !!}
+@else
+    {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm"]) !!}
+    {!! Form::close() !!}
 @endif
